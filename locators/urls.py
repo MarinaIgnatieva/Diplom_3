5a75fc24f7641ee0600aff644d0cb054c4503f73
Marina Ignatieva <chita-girl@yandex.ru>
2025-07-13 17:38:00 +0700

    добавлены автотесты для UI

---
+++ locators/urls.py
@@ -0,0 +1,14 @@
+# Главная страница
+MAIN_PAGE_BURGER_URL = "https://stellarburgers.nomoreparties.site/"
+
+# Страница с Лентой заказов
+ORDER_FEED_PAGE_URL = "https://stellarburgers.nomoreparties.site/feed"
+
+# Страница Личного кабинете
+PERSONAL_ACCOUNT_PAGE_URL = "https://stellarburgers.nomoreparties.site/account/profile"
+
+# Страница Авторизации
+LOGIN_BURGER_URL = "https://stellarburgers.nomoreparties.site/login"
+
+# Страница Регистрации
+REGISTER_BURGER_URL = "https://stellarburgers.nomoreparties.site/register"
